a829a72d20b873fbfb0d17adfa5ac52a1f54a512
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-28 15:50:06 -0600

    updated comment.js file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = express.Router();
-const { User } = require("../models");
+const express = require('express').Router();
+const { User, Post, Comment } = require('../models');
 
-// Define routes
+//Define routes
 //Get all users
 
 //get one single user
@@ -10,19 +9,44 @@ const { User } = require("../models");
 //POST route for user updates
 
 //POST route for user login
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that email exists.' });
-            return;
-        }
-    })
-})
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 
 
