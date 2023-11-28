53e452c7f05642124fe0ab3501ac7c278ff7ae0c
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-28 02:41:49 -0600

    updated userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,5 +3,29 @@ const router = express.Router();
 const { User } = require("../models");
 
 // Define routes
+//Get all users
+
+//get one single user
+
+//POST route for user updates
+
+//POST route for user login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email exists.' });
+            return;
+        }
+    })
+})
+
+
+
+
 
 module.exports = router;
