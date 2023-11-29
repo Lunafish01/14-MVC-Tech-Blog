941818bb72afe0f4a346161695af77c926939895
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-29 01:38:48 -0600

    updated userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,57 @@
-const express = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const express = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const { User, Post, Comment } = require("../models");
 
-//Define routes
+//define routes
 
-//Get all users
+//get all users
+//define a route for handling GET requests to '/api/users'
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-//get one single user
+//get single user
+//define a route for handling GET requests to '/api/users/:id'
+router.get("/:id", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      attributes: { exclude: ["password"] },
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ["id", "title", "post_content", "created_at"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "created_at"],
+          include: {
+            model: Post,
+            attributes: ["title"],
+          },
+        },
+      ],
+    });
+    if (!dbUserData) {
+      res.status(404).json({ message: "User not found!" });
+      return;
+    }
+    res.json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 //POST route for user updates
 
