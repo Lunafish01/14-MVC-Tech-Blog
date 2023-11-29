b571e69a8a8ef0be1162d7b27b074ef777391b83
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-29 01:07:55 -0600

    updated helpers.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
-const session = require('express-sessions');
+const session = require("express-sessions");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 // Set up sessions with cookies
 //remember to use an env variable to store this data
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     // Stored in milliseconds
     //setting a maximum age for this session
@@ -35,13 +34,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Handlebars setup
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // Routes
 app.use(routes);
 
 // Start server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+  );
 });
