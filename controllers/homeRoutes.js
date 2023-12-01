5eb3f3a1f70b33f50110e5f9adb09e970fbf5e1b
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-30 20:25:01 -0600

    updated dependencies

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,34 @@
-const express = require("express");
-const router = express.Router();
+const router = require('express').Router();
 const { Post, User, Comment } = require("../models");
+const sequelize = require('../config/connection');
 
-// Define routes
+//GET request to get all posts
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      attributes: ["id", "title", "created_at", "post_content"],
+    //   order: [["created_at", "DESC"]],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "title", "created_at", "post_content"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    res.json(dbPostData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
