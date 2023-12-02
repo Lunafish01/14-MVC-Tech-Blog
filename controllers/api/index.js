f882e8e8aa0be1de208b6d66813db0f03e209509
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-12-02 13:52:21 -0600

    updated controllers and models folders

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const postRoutes = require("./postRoutes");
 const commentRoutes = require("./commentRoutes");
 
 router.use("/Users", userRoutes);
-router.use("/Post", postRoutes);
-router.use("/Comment", commentRoutes);
+router.use("/Posts", postRoutes);
+router.use("/Comments", commentRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const homeRoutes = require('./homeRoutes');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
+
 router.use((req, res) => {
     res.status(404).end();  
 });
